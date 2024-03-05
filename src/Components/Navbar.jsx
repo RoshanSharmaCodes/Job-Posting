16c9472fef9885642707b4e1c5d490ff9acf9ece
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-03-06 00:42:04 +0530

    * Responsive Navbar Completed.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react"
 import { Link, NavLink } from "react-router-dom"
+import { FaAlignJustify, FaXmark } from "react-icons/fa6";
 
 export default function Navbar() {
   const [isModalOpen, setModalOpen] = useState(false)
 
-  const toggleModal = () => {
+  const handleMenuToggle = () => {
     setModalOpen(!isModalOpen)
   }
 
@@ -29,11 +30,15 @@ export default function Navbar() {
   return (
     <header className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
       <nav className="flex justify-between items-center py-6">
+
+        {/* Main Logo */}
         <a href="/" className="flex items-center gap-2 text-2xl">
           <span>
             <span className="text-blue">Remotely</span>Works
           </span>
         </a>
+
+        {/* NavBar Menu Items */}
         <ul className="hidden md:flex gap-12">
           {navItems.map(({path, title}) => (
             <li key={path} className="text-base text-primary">
@@ -48,11 +53,40 @@ export default function Navbar() {
             </li>
           ))}
         </ul>
+
+        {/* Action Buttons */}
         <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
             <Link to={"/LogIn"} className="py-2 px-5 border rounded">Log In</Link>
             <Link to={"/SignUp"} className="py-2 px-5 border rounded bg-blue text-white">Sign Up</Link>
         </div>
+        
+        {/* Menu Bar for Mobile */}
+        <div className="md:hidden block">
+            <button onClick={handleMenuToggle}>{isModalOpen? <FaXmark className="w-5 h-5 text-primary"/> : <FaAlignJustify className="w-5 h-5 text-primary"/>}</button>
+        </div>
+
+     
+
       </nav>
+         {/* Navbar for Mobile */}
+         <div className={`px-4 bg-blue py-5 rounded-sm ${isModalOpen?"text-white":"hidden"}`}>
+            <ul>
+            {navItems.map(({path, title}) => (
+            <li key={path} className=" text-white py-1">
+              <NavLink
+                to={path}
+                className={({ isActive }) => {
+                  isActive ? "active text-white" : ""
+                }}
+              >
+                {title}
+              </NavLink>
+            </li>
+          ))}
+          <Link to={"/LogIn"} className=" text-white py-1">Log In</Link>
+            </ul>
+             
+        </div>
     </header>
   )
 }
