7c2be1765e108a69517d8f26f4457875abc7d663
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-03-05 23:28:40 +0530

    * Dummy Data Added.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   return (
     <>
-    <h1 className='text-3xl font-bold underline'>What's up fellas?</h1>
+    <h1 className='text-blue'>What's up fellas?</h1>
     <Outlet/>
     </>
   )
