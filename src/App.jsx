903daa664508dd9e367b1e996c697d749eb81840
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-03-05 21:36:14 +0530

    * Added Pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
-    <h1 className='text-3xl font-bold underline'>Whatsup</h1>
+    <h1 className='text-3xl font-bold underline'>What's up fellas?</h1>
+    <Outlet/>
     </>
   )
 }
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,14 +1,22 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom"
+import App from "../App"
+import Home from "../Pages/Home"
 
 const Router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
         path: "/",
-        element: ""
-    },
-    {
-        path: "",
-        element: ""
-    }
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "",
+    element: "",
+  },
 ])
 
 export default Router
