ec79dc45854bd431f63baa215be29eadb5100ccb
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-03-08 00:26:12 +0530

    * Job Posting Cards Finished.

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiCalendar, FiClock, FiDollarSign, FiMapPin } from "react-icons/fi";
 
 export default function Card({data}) {
   return (
-    <div>{data.jobTitle}</div>
+    <section className='card'>
+      <Link to={"/"} className='flex gap-4 flex-col sm:flex-row items-start'>
+        <img src={data.companyLogo} alt={data.companyName} />
+        <div>
+          <h4 className='text-primary mb-1'>{data.companyName}</h4>
+          <h3 className='text-lg font-semibold mb-2'>{data.jobTitle}</h3>
+
+          <div className='flex flex-wrap gap-2 mb-2 text-base text-primary/70'>
+            <span className='flex items-center gap-1'><FiMapPin/>{data.jobLocation}</span>
+            <span className='flex items-center gap-1'><FiClock/>{data.employmentType}</span>
+            <span className='flex items-center gap-1'><FiDollarSign/>{data.minPrice}-{data.maxPrice} K</span>
+            <span className='flex items-center gap-1'><FiCalendar/>{data.postingDate}</span>
+          </div>
+
+          <p className='text-base text-primary/70'>{data.description}</p>
+        </div>
+      </Link>
+    </section>
   )
 }
--- src/Pages/Jobs.jsx
+++ src/Pages/Jobs.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Jobs({cardData}) {
   return (
     <div>
+      <h1 className='text-2xl'>Found: {cardData.length}</h1>
         {cardData}
     </div>
   )
