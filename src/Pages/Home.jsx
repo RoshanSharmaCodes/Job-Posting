511f33398c2ae297dcc1b336f07a1dd946a8e93b
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-03-10 00:42:43 +0530

    * Pagination Added to Job Post Section.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -34,6 +34,25 @@ export default function Home() {
     setSelectedCategory(e.target.value)
   }
 
+  const getJobPageIndex = () => {
+    const startIdx = itemsPerPage * (currentPage - 1)
+    const endIdx = itemsPerPage + startIdx
+    return {startIdx, endIdx}
+  } 
+
+  const nextPage = () => {
+    if(currentPage < Math.ceil(filterItems.length / itemsPerPage))
+    {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  const prevPage = () => {
+    if(currentPage > 1){
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
   const filterData = (job, selected, query) => {
     console.log("Filter Data Function Called")
     let filteredJobs = jobs
@@ -53,6 +72,8 @@ export default function Home() {
       )
     }
 
+    const {startIdx, endIdx} = getJobPageIndex()
+    filteredJobs = filteredJobs.slice(startIdx, endIdx)
     return filteredJobs.map((data, i) => <Card key={i} data={data} />)
   }
 
@@ -65,7 +86,16 @@ export default function Home() {
       {/* Main Content */}
       <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
         <div className="bg-white p-4 rounded"><Sidebar handleChangeCategory={handleChangeCategory} handleClickCategory={handleClickCategory} /></div>
-        <div className="bg-white col-span-2 p-4 rounded-sm"><Jobs cardData={results} /></div>
+        <div className="bg-white col-span-2 p-4 rounded-sm">
+          {results.length > 0? <Jobs cardData={results} /> : "Oops! No Data Found..."}
+          {
+            results.length > 0 ? <div className="flex justify-center mt-4 space-x-8">
+            <button onClick={prevPage} className="hover:underline" disabled={currentPage == 1}>Previous</button>
+            <span className="mx-2">Page {currentPage} of {Math.ceil(filterItems.length / itemsPerPage)}</span>
+            <button onClick={nextPage} className="hover:underline" disabled={currentPage == Math.ceil(filterItems.length/itemsPerPage)}>Next</button>
+          </div> : ""
+          }
+        </div>
         <div className="bg-white p-4 rounded">Right</div>
       </div>
     </div>
--- src/Sidebar/Location.jsx
+++ src/Sidebar/Location.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import RadioInput from '../Components/RadioInput'
+import React, { useEffect, useState } from "react"
+import RadioInput from "../Components/RadioInput"
 
-export default function Location({handleChangeCategory}){
-
+export default function Location({ handleChangeCategory }) {
   return (
     <div>
-        <h4 className='text-lg font-medium mb-2'>Location</h4>
-        <div>
-                <RadioInput handleChangeCategory={handleChangeCategory} title={"London"} value={"London"} name={"test"} />
-                <RadioInput handleChangeCategory={handleChangeCategory} title={"Mumbai"} value={"Mumbai"} name={"test"} />
-        </div>
+      <h4 className="text-lg font-medium mb-2">Location</h4>
+
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" value="" onChange={handleChangeCategory} />
+          <span className="checkmark"></span> All
+        </label>
+        
+        <RadioInput handleChangeCategory={handleChangeCategory} title={"London"} value={"London"} name={"test"} />
+        <RadioInput handleChangeCategory={handleChangeCategory} title={"Mumbai"} value={"Mumbai"} name={"test"} />
+      </div>
     </div>
   )
 }
